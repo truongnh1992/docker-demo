d753daf0970edfa3411ad3cc4aba364ea17280df
Alex Nguyen <truong@clare.ai>
2023-04-14 17:13:28 +0700

    Update output message

--- go/main.go
+++ go/main.go
@@ -21,6 +21,6 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Serving request: %s", r.URL.Path)
-	fmt.Fprintf(w, "Hello, Google Developer Student Clubs - PTIT\n")
+	fmt.Fprintf(w, "Hello QuanQuanGCP Season 5\n")
 	fmt.Fprintf(w, "Version: 1.0.0\n")
 }
