fdb5cc02339e367bb09b9f288f3426831addb1ba
Alex Nguyen <truong@clare.ai>
2023-04-24 09:57:51 +0700

    Test Include files Cloud Build Trigger

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ const port = 80;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
       res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, GDSC PTIT\n');
+        res.end('Hello, QuanQuanGCP\n');
 });
 server.listen(port, hostname, () => {
     console.log('Server running at http://%s:%s/', hostname, port);
